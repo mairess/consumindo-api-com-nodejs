0fba2e391f9a28ae52c49b44bc4ddc8c6cf2f4ed
Maires Souza <trabalho.maires@gmail.com>
2023-04-24 10:27:10 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 // const express = require('express')
-
+const axios = require('axios')
 const app = express()
 
 app.listen('3000')
 
 // app.use(express.json())
 
-app.route('/').get((req, res) => res.send('Olá meu camarada'))
+app.route('/').get((req, res) => {
+    axios.get('https://api.github.com/users/mairess')
+    .then( result => res.send(`<img src="${result.data.avatar_url}"/>`))
+    .catch( error => console.log(error))
+})
